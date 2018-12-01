0cac501b346f3f68c2d82206e678a455906d4b6d
Benjamin Cohen <bac1087@gmail.com>
2018-12-01 15:39:53 -0600

    modified top tab  nav

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -7,6 +7,8 @@ import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
+import { View, Text } from 'react-native';
+
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
 });
@@ -28,6 +30,19 @@ HomeStack.navigationOptions = {
 const LinksStack = createMaterialTopTabNavigator({
   Links: LinksScreen,
   Another: SettingsScreen
+}, {
+  tabBarOptions: {
+    labelStyle: {
+      fontSize: 12,
+    },
+    tabStyle: {
+      width: 160,
+      height: 80
+    },
+    style: {
+      backgroundColor: 'blue',
+    },
+  }
 });
 
 LinksStack.navigationOptions = {
